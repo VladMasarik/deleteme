b3d390771d190c9bc6215105620014dbf5650e09
Vladimir Masarik <masarik.vladimir7@gmail.com>
2022-02-12 15:03:11 +0100

    aaa

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"rsc.io/quote"
@@ -21,9 +23,10 @@ func main() {
 		panic(err.Error())
 	}
 	// pods, err := clientset.CoreV1().Pods("")
-	pods := clientset.CoreV1().Pods("")
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("There are %d pods in the cluster\n", pods)
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	fmt.Printf("There are %d pods in the cluster\n", pods.Items)
 }
